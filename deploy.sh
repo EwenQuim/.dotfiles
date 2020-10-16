8546129f96b5af14810c37a0190342246d1c74cc
EwenQuim <ewen.quimerch@gmail.com>
2020-10-16 12:50:24 +0200

    New conf for dotfiles

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,27 @@
-cp .zshrc ~/.zshrc
-cp custom.zsh-theme .oh-my-zsh/custom/themes/
+git pull
+git diff
+
+
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure to apply the new dotfile conf? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+confirm &&
+
+git commit -a -v && 
+git push origin master &&
+
+cp zsh/.zshrc ~/.zshrc &&
+
+cp zsh/custom.zsh-theme .oh-my-zsh/custom/themes/ &&
+
 zsh
