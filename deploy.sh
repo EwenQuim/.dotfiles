184b8b1c1bd01ccf46ddb2a8c8b7f412156f2cd7
EwenQuim <ewen.quimerch@gmail.com>
2020-10-16 15:30:00 +0200

    Deployment

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ normal=$(tput sgr0)
 set -e
 
 echo "\n${bold}> Pulling remote conf${normal}"
-git pull;
+git pull; git diff
 
-read response\?"Are you sure? [y/N] "
+read response\?"${bold}> Are you sure to apply dotfile changes? [y/N] "
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]] ; then
     git commit -a -v
 
@@ -16,11 +16,11 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]] ; then
     git push origin master
 
     echo "\n${bold}> Copying zsh & oh-my-zsh conf${normal}"
-    cp zsh/.zshrc ~/.zshrc
-    cp zsh/custom.zsh-theme ~/.oh-my-zsh/custom/themes/
-    zsh
+    cp -v zsh/.zshrc ~/.zshrc
+    cp -v zsh/custom.zsh-theme ~/.oh-my-zsh/custom/themes/
 
     echo "\n${bold}> Everything is synchronized${normal}"
+    zsh
 else
   exit 0
 fi
